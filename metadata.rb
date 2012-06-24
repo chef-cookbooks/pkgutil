5c51999951f6ce8af755b1777db0687e45518904
Martha Greenberg <marthag@wix.com>
2012-06-24 16:38:05 +0300

    fix email to wix.com address

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Martha Greenberg"
-maintainer_email "marthag@mit.edu"
+maintainer_email "marthag@wix.com"
 license          "Apache 2.0"
 description      "Manages Solaris pkgutil packages"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
