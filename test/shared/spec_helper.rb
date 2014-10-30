85c714c30635de556d9aa998c99f50a4b5db9813
Yvonne Lam <yvonne@opscode.com>
2014-10-30 14:44:11 -0700

    Start writing serverspec tests.

---
+++ test/shared/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'serverspec'
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |file| require_relative(file) }
+
+RSpec.configure do |config|
+  config.before(:all) do
+    config.os = backend(Serverspec::Commands::Base).check_os
+  end
+end
