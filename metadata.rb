9c1d3c1425240bea2707bf861bc3670391773e8c
Tim Smith <tsmith84@gmail.com>
2015-09-08 21:00:47 -0700

    Add name and supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,4 @@
+name             'pkgutil'
 maintainer       "Martha Greenberg"
 maintainer_email "marthag@wix.com"
 license          "Apache 2.0"
@@ -6,3 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.3"
 
 supports "solaris2"
+source_url 'https://github.com/chef-cookbooks/pkgutil' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/pkgutil/issues' if respond_to?(:issues_url)
