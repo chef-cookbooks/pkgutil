044111952eb5681f035914a113ca1a7e14aa0b94
Tim Smith <tsmith84@gmail.com>
2016-04-27 10:08:56 -0700

    Update maintainer contact info

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,12 @@
 name             'pkgutil'
-maintainer       'Martha Greenberg'
-maintainer_email 'marthag@wix.com'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Manages Solaris pkgutil packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.3'
 
 supports 'solaris2'
+
 source_url 'https://github.com/chef-cookbooks/pkgutil' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/pkgutil/issues' if respond_to?(:issues_url)
