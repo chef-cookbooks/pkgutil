28382a6532eff588837f3dc2b8a859f3531a8424
Tim Smith <tsmith@chef.io>
2017-02-23 20:21:42 -0800

    Fix compatibility with older Chef 12 clients
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -10,5 +10,4 @@ supports 'solaris2'
 
 source_url 'https://github.com/chef-cookbooks/pkgutil'
 issues_url 'https://github.com/chef-cookbooks/pkgutil/issues'
-
-chef_version '>= 12.1'
+chef_version '>= 12.1' if respond_to?(:chef_version)
