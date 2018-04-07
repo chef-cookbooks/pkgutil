dfcd9392a77c11ebcd42242a3b262b9dca93c755
Tim Smith <tsmith@chef.io>
2018-04-06 23:00:47 -0400

    Remove matchers that are auto generated now
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/matchers.rb
+++
@@ -1,9 +0,0 @@
-if defined?(ChefSpec)
-  def add_pkgutil_repository(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:pkgutil_repository, :add, resource_name)
-  end
-
-  def remove_pkgutil_repository(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:pkgutil_repository, :remove, resource_name)
-  end
-end
