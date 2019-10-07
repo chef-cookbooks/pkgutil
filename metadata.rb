9353a91435a1fd595845306cfc66fd312138c37b
Tim Smith <tsmith84@gmail.com>
2019-10-07 15:34:30 -0700

    Resolve Cookstyle 5.8 warnings
    Minor cleanups for the latest Cookstyle
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,10 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Manages Solaris pkgutil packages'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.0.0'
 
 supports 'solaris2'
 
 source_url 'https://github.com/chef-cookbooks/pkgutil'
 issues_url 'https://github.com/chef-cookbooks/pkgutil/issues'
-chef_version '>= 12.7' if respond_to?(:chef_version)
+chef_version '>= 12.7'
